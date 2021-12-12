2f379424d2c71f71816c651b1879492066c94c61
Dhonatan <dhon.joseildo@gmail.com>
2021-12-11 23:04:54 -0300

    update 1.2

--- modal.js
+++ modal.js
@@ -1,7 +1,6 @@
 
 function closeModal(){
   const bgModal = document.querySelector('.bgModal');
-  const containerModal = document.querySelector('.containerModal')
 
   bgModal.style.display = "none"
 }
